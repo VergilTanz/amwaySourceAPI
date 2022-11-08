d1e796bfe8b8f543c2c7d531be1102b24f4eba80
Vergil Tan <myu0777p@MYWSICJ508.local>
2022-11-08 18:56:44 +0800

    first complete entry to mysql databases

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 const functions = require('@google-cloud/functions-framework');
-const escapeHtml = require('escape-html');
-
-/**
- * Responds to an HTTP request using data from the request body parsed according
- * to the "content-type" header.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
-// functions.http('helloHttp', (req, res) => {
-//     res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
-// });
+const mysql = require('mysql')
 
 functions.http('helloHttp', (req, res) => {
 
@@ -20,14 +9,16 @@ functions.http('helloHttp', (req, res) => {
     console.log('This is for helloHttp logging purpose')
     console.log('This is for helloHttp logging purpose')
     console.log('This is for helloHttp logging purpose')
-    console.log(req.query)
-    console.log('-------------------------------------')
-    console.log(req.query.name)
-    console.log('-------------------------------------')
     console.log(req.body)
     console.log('-------------------------------------')
-    console.log(req.body.name)
-    console.log('-------------------------------------')
+
+    const pool = mysql.createPool({
+        connectionLimit: 1,
+        host: '104.197.196.36',
+        port: '3306',
+        user: 'root',
+        database: 'source'
+    })
 
     res.set('Access-Control-Allow-Origin', '*')
 
@@ -38,8 +29,56 @@ functions.http('helloHttp', (req, res) => {
         res.set('Access-Control-Max-Age', '3600');
         res.status(204).send('Hello World with Options 2');
     } else {
-        res.send('Hello World! without Options 2');
-    }
+        if (req.body === undefined) {
+            res.send('Error : missing inputs');
+        }
 
-    // res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
+
+
+        const values = {
+            productNumber: `${req.body.skuPrefix}${req.body.skuRoot}${req.body.skuSuffix}`,
+            email: req.body.email,
+            percentage: req.body.percentage,
+            effectiveDate: req.body.effectiveDate,
+            productStatus: req.body.status,
+            effectiveTime: req.body.effectiveTime,
+            userPassword: req.body.password,
+            businessLine: req.body.businessLine,
+            productCategory: req.body.productCategory,
+            productSubCategory: req.body.productSubCategory
+        }
+
+        // error on purpose
+        pool.query(`select * from example where productNumber = 'AABBCCCDDEESS'`, (error, results) => {
+            console.log('error on purpose')
+            console.log(results)
+        })
+
+        // return error if product exist
+        pool.query(`select * from example where productNumber = '${values.productNumber}'`, (error, results) => {
+            if (results.length > 0) {
+                res.send(JSON.stringify(results));
+            }
+        })
+
+        const query = `INSERT INTO example
+        VALUES (
+            "${values.productNumber}",
+            "${values.email}",
+            "${values.percentage}",
+            "98-09-04",
+            "${values.productStatus}",
+            "22:33:44",
+            "${values.userPassword}",
+            "${values.businessLine}",
+            "${values.productCategory}",
+            "${values.productSubCategory}")`
+
+        pool.query(query, (error, results) => {
+            console.log('inserting data...')
+            console.log(error)
+            console.log(results)
+            res.send('data insert successfully');
+        })
+    }
 });
